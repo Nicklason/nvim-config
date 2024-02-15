f80b96d062cf61ec66a7e6fbf35559b915b6eab2
Nicklason <Nicklason@users.noreply.github.com>
2024-02-15 17:16:53 +0100

    feat: use gitsigns

--- after/plugin/neoformat.lua
+++ after/plugin/neoformat.lua
@@ -1,3 +1,10 @@
+--vim.cmd([[
+--  augroup NeoformatDefaultBuffer
+--    autocmd!
+--    autocmd BufWritePre <buffer=1> Neoformat
+--  augroup END
+--]])
+
 vim.api.nvim_create_autocmd("BufWritePre", {
     command = "Neoformat",
 })
